41ee3a946bed0c946a33bd47980ee3faca6d986f
louib <code@louib.net>
2021-05-08 16:11:30 -0400

    Engage debian discovery

--- fpm-tools/bin/fpm-tools.rs
+++ fpm-tools/bin/fpm-tools.rs
@@ -118,7 +118,18 @@ fn main() {
             // mine_repository(&mut db, &gitlab_repo_url);
         }
 
-        // get_gitlab_repos("salsa.debian.org", "FPM_DEBIAN_GITLAB_TOKEN");
+        let gitlab_repo_urls = match get_gitlab_repos("salsa.debian.org", "FPM_DEBIAN_GITLAB_TOKEN") {
+            Ok(r) => r,
+            Err(e) => panic!(e),
+        };
+        for gitlab_repo_url in gitlab_repo_urls.split('\n') {
+            if gitlab_repo_url.trim().is_empty() {
+                continue;
+            }
+            eprintln!("repo url is {}", gitlab_repo_url);
+            // mine_repository(&mut db, &gitlab_repo_url);
+        }
+
         // get_gitlab_repos("invent.kde.org", "FPM_KDE_GITLAB_TOKEN");
         // get_gitlab_repos("gitlab.freedesktop.org", "FPM_XDG_GITLAB_TOKEN");
         // get_gitlab_repos("code.videolan.org", "FPM_VLC_GITLAB_TOKEN");
