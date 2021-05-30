e961301dbfb9b71c3c42a0f4e1c25b22c49176c9
louib <code@louib.net>
2021-05-30 15:11:09 -0400

    Adding stats for depth of manifests.

--- fpm-tools/bin/fpm-stats.rs
+++ fpm-tools/bin/fpm-stats.rs
@@ -15,6 +15,8 @@ fn main() {
     let mut invalid_sources_count: i64 = 0;
     let mut empty_sources_count: i64 = 0;
     let mut modules_count: i64 = 0;
+    let mut manifests_max_depth: BTreeMap<i32, i64> = BTreeMap::new();
+    let mut manifests_count: i64 = 0;
     let mut patched_modules_count: i64 = 0;
 
     for (project_id, project) in &db.indexed_projects {
@@ -51,6 +53,11 @@ fn main() {
 
 
             if let Some(flatpak_manifest) = FlatpakManifest::load_from_file(file_path.to_string()) {
+                manifests_count += 1;
+
+                let manifest_depth = flatpak_manifest.get_max_depth();
+                let new_count = manifests_max_depth.get(&manifest_depth).unwrap_or(&0) + 1;
+                manifests_max_depth.insert(manifest_depth, new_count);
                 println!("MANIFEST MAX DEPTH {} {}", flatpak_manifest.get_max_depth(), file_path);
 
                 for module in &flatpak_manifest.modules {
@@ -65,10 +72,11 @@ fn main() {
 
                     if let FlatpakModule::Description(d) = module {
                         for source in &d.sources {
+                            sources_total_count += 1;
+
                             let source_type_name = source.get_type_name();
                             let new_count = sources_count.get(&source_type_name).unwrap_or(&0) + 1;
                             sources_count.insert(source_type_name, new_count);
-                            sources_total_count += 1;
 
                             if !source.type_is_valid() {
                                 invalid_sources_count += 1;
@@ -92,6 +100,12 @@ fn main() {
 
         }
     }
+    println!("Manifests:");
+    for (depth, depth_count) in manifests_max_depth {
+        println!("Depth {}: {} ({}/{})%", depth, (depth_count as f64 / manifests_count as f64) * 100.0, depth_count, manifests_count);
+    }
+    println!("\n");
+
     println!("Modules:");
     println!("Patched modules: {} ({}/{})%", (patched_modules_count as f64 / modules_count as f64) * 100.0, patched_modules_count, modules_count);
     println!("\n");
