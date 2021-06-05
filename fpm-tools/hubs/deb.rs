a7d9fe9e2c6f85d9485445090a69dbf123bd4ee6
louib <code@louib.net>
2021-06-04 20:28:08 -0400

    Maybe a working debian discovery

--- fpm-tools/hubs/deb.rs
+++ fpm-tools/hubs/deb.rs
@@ -1,3 +1,5 @@
+use std::path;
+
 pub fn get_all_repos(repo_sources_url: &str) -> Result<Vec<String>, String> {
     log::info!("Getting debian repos {}", repo_sources_url);
 
@@ -6,6 +8,27 @@ pub fn get_all_repos(repo_sources_url: &str) -> Result<Vec<String>, String> {
         Err(e) => return Err(e),
     };
 
+    let debian_sources_dir_path = match fpm::utils::uncompress(&debian_sources_file_path) {
+        Ok(p) => p,
+        Err(e) => return Err(e),
+    };
+
+    let debian_sources_file_paths = match fpm::utils::get_all_paths(path::Path::new(&debian_sources_dir_path)) {
+        Ok(paths) => paths,
+        Err(message) => return Err(message),
+    };
+
+    for file_path in &debian_sources_file_paths {
+        if !file_path.is_file() {
+            continue;
+        }
+
+        let file_path = match file_path.to_str() {
+            Some(f) => f,
+            None => continue,
+        };
+        log::info!("Parsing Debian source file {}", file_path);
+    }
 
 
     Ok(vec![])
