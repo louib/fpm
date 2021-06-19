4b060fb4be8188afaa2aee6fae48460839a9050d
louib <code@louib.net>
2021-06-19 19:01:31 -0400

    Only parse known manifests.

--- fpm-tools/bin/fpm-stats.rs
+++ fpm-tools/bin/fpm-stats.rs
@@ -58,172 +58,146 @@ fn main() {
                 continue;
             },
         };
-        let repo_file_paths = match fpm::utils::get_all_paths(path::Path::new(&repo_dir)) {
-            Ok(paths) => paths,
-            Err(message) => {
-                log::error!("Could not get the file paths for {} :sad: {}", repo_dir, message);
-                continue;
-            }
-        };
 
-        for file_path in &repo_file_paths {
-            if !file_path.is_file() {
-                continue;
-            }
+        for manifest_path in &project.flatpak_app_manifests {
+            let absolute_manifest_path = repo_dir.to_string() + manifest_path;
 
-            let file_path = match file_path.to_str() {
-                Some(f) => f,
-                None => continue,
-            };
-            let file_relative_path = file_path.replace(&repo_dir, "");
-            if (
-              !project.flatpak_app_manifests.contains(&file_relative_path) &&
-              !project.flatpak_module_manifests.contains(&file_relative_path)
-            ) {
-                continue;
+            let flatpak_manifest = FlatpakManifest::load_from_file(absolute_manifest_path).unwrap();
+            manifests_count += 1;
+
+            app_ids.insert(flatpak_manifest.get_id());
+
+            if flatpak_manifest.is_extension() {
+                extension_manifests_count += 1;
             }
 
-            if file_path.contains(".git/") {
-                continue;
+            for extension_name in &flatpak_manifest.sdk_extensions {
+                let new_count = extensions_count.get(extension_name).unwrap_or(&0) + 1;
+                extensions_count.insert(extension_name.to_string(), new_count);
+            }
+            if flatpak_manifest.sdk_extensions.len() == 0 {
+                no_extensions_count += 1;
             }
 
-            if let Some(flatpak_manifest) = FlatpakManifest::load_from_file(file_path.to_string()) {
-                manifests_count += 1;
+            let manifest_depth = flatpak_manifest.get_max_depth();
+            let new_count = manifests_max_depth.get(&manifest_depth).unwrap_or(&0) + 1;
+            manifests_max_depth.insert(manifest_depth, new_count);
 
-                app_ids.insert(flatpak_manifest.get_id());
+            for module in &flatpak_manifest.modules {
+                let module_description = match &module {
+                    FlatpakModule::Path(_) => continue,
+                    FlatpakModule::Description(d) => d,
+                };
 
-                if flatpak_manifest.is_extension() {
-                    extension_manifests_count += 1;
-                }
-
-                for extension_name in &flatpak_manifest.sdk_extensions {
-                    let new_count = extensions_count.get(extension_name).unwrap_or(&0) + 1;
-                    extensions_count.insert(extension_name.to_string(), new_count);
-                }
-                if flatpak_manifest.sdk_extensions.len() == 0 {
-                    no_extensions_count += 1;
-                }
-
-                let manifest_depth = flatpak_manifest.get_max_depth();
-                let new_count = manifests_max_depth.get(&manifest_depth).unwrap_or(&0) + 1;
-                manifests_max_depth.insert(manifest_depth, new_count);
-
-                for module in &flatpak_manifest.modules {
-                    let module_description = match &module {
-                        FlatpakModule::Path(_) => continue,
-                        FlatpakModule::Description(d) => d,
-                    };
-
-                    // We're only counting inlined module descriptions for now.
-                    modules_count += 1;
-                    for url in module.get_all_repos_urls() {
-                        let url = url.trim();
-                        modules_urls_count += 1;
-                        if url == "." || url == ".." || url.starts_with("./") || url.starts_with("../") {
-                            let new_modules_protocol_count = modules_urls_protocols.get("relative fs path").unwrap_or(&0) + 1;
-                            modules_urls_protocols.insert("relative fs path".to_string(), new_modules_protocol_count);
-                        } else if url.starts_with("http://") {
-                            let new_modules_protocol_count = modules_urls_protocols.get("http").unwrap_or(&0) + 1;
-                            modules_urls_protocols.insert("http".to_string(), new_modules_protocol_count);
-                        } else if url.starts_with("https://") {
-                            let new_modules_protocol_count = modules_urls_protocols.get("https").unwrap_or(&0) + 1;
-                            modules_urls_protocols.insert("https".to_string(), new_modules_protocol_count);
-                        } else if url.starts_with("git://") {
-                            let new_modules_protocol_count = modules_urls_protocols.get("git").unwrap_or(&0) + 1;
-                            modules_urls_protocols.insert("git".to_string(), new_modules_protocol_count);
-                        } else if url.starts_with("ftp://") {
-                            let new_modules_protocol_count = modules_urls_protocols.get("ftp").unwrap_or(&0) + 1;
-                            modules_urls_protocols.insert("ftp".to_string(), new_modules_protocol_count);
-                        } else if url.starts_with("svn://") {
-                            let new_modules_protocol_count = modules_urls_protocols.get("svn").unwrap_or(&0) + 1;
-                            modules_urls_protocols.insert("svn".to_string(), new_modules_protocol_count);
-                        } else if url.starts_with("file://") {
-                            let new_modules_protocol_count = modules_urls_protocols.get("file").unwrap_or(&0) + 1;
-                            modules_urls_protocols.insert("file".to_string(), new_modules_protocol_count);
-                        } else {
-                            let new_modules_protocol_count = modules_urls_protocols.get("other").unwrap_or(&0) + 1;
-                            modules_urls_protocols.insert("other".to_string(), new_modules_protocol_count);
-                            log::warn!("UNKNOWN URL PROTOCOL {}", url);
-                        }
-                    }
-
-                    for url in module.get_repos_mirror_urls() {
-                        modules_mirror_urls_count += 1;
-                    }
-
-                    if module.is_patched() {
-                        patched_modules_count += 1;
-                    }
-
-                    let module_sources_count = module.get_sources_count() as i32;
-                    let new_sources_count = modules_sources_count.get(&module_sources_count).unwrap_or(&0) + 1;
-                    modules_sources_count.insert(module_sources_count, new_sources_count);
-
-                    if let Some(buildsystem) = module_description.get_buildsystem() {
-                        let new_buildsystem_count = modules_buildsystems_count.get(&buildsystem).unwrap_or(&0) + 1;
-                        modules_buildsystems_count.insert(buildsystem.to_string(), new_buildsystem_count);
+                // We're only counting inlined module descriptions for now.
+                modules_count += 1;
+                for url in module.get_all_repos_urls() {
+                    let url = url.trim();
+                    modules_urls_count += 1;
+                    if url == "." || url == ".." || url.starts_with("./") || url.starts_with("../") {
+                        let new_modules_protocol_count = modules_urls_protocols.get("relative fs path").unwrap_or(&0) + 1;
+                        modules_urls_protocols.insert("relative fs path".to_string(), new_modules_protocol_count);
+                    } else if url.starts_with("http://") {
+                        let new_modules_protocol_count = modules_urls_protocols.get("http").unwrap_or(&0) + 1;
+                        modules_urls_protocols.insert("http".to_string(), new_modules_protocol_count);
+                    } else if url.starts_with("https://") {
+                        let new_modules_protocol_count = modules_urls_protocols.get("https").unwrap_or(&0) + 1;
+                        modules_urls_protocols.insert("https".to_string(), new_modules_protocol_count);
+                    } else if url.starts_with("git://") {
+                        let new_modules_protocol_count = modules_urls_protocols.get("git").unwrap_or(&0) + 1;
+                        modules_urls_protocols.insert("git".to_string(), new_modules_protocol_count);
+                    } else if url.starts_with("ftp://") {
+                        let new_modules_protocol_count = modules_urls_protocols.get("ftp").unwrap_or(&0) + 1;
+                        modules_urls_protocols.insert("ftp".to_string(), new_modules_protocol_count);
+                    } else if url.starts_with("svn://") {
+                        let new_modules_protocol_count = modules_urls_protocols.get("svn").unwrap_or(&0) + 1;
+                        modules_urls_protocols.insert("svn".to_string(), new_modules_protocol_count);
+                    } else if url.starts_with("file://") {
+                        let new_modules_protocol_count = modules_urls_protocols.get("file").unwrap_or(&0) + 1;
+                        modules_urls_protocols.insert("file".to_string(), new_modules_protocol_count);
                     } else {
-                        let new_buildsystem_count = modules_buildsystems_count.get("unspecified").unwrap_or(&0) + 1;
-                        modules_buildsystems_count.insert("unspecified".to_string(), new_buildsystem_count);
+                        let new_modules_protocol_count = modules_urls_protocols.get("other").unwrap_or(&0) + 1;
+                        modules_urls_protocols.insert("other".to_string(), new_modules_protocol_count);
+                        log::warn!("UNKNOWN URL PROTOCOL {}", url);
+                    }
+                }
+
+                for url in module.get_repos_mirror_urls() {
+                    modules_mirror_urls_count += 1;
+                }
+
+                if module.is_patched() {
+                    patched_modules_count += 1;
+                }
+
+                let module_sources_count = module.get_sources_count() as i32;
+                let new_sources_count = modules_sources_count.get(&module_sources_count).unwrap_or(&0) + 1;
+                modules_sources_count.insert(module_sources_count, new_sources_count);
+
+                if let Some(buildsystem) = module_description.get_buildsystem() {
+                    let new_buildsystem_count = modules_buildsystems_count.get(&buildsystem).unwrap_or(&0) + 1;
+                    modules_buildsystems_count.insert(buildsystem.to_string(), new_buildsystem_count);
+                } else {
+                    let new_buildsystem_count = modules_buildsystems_count.get("unspecified").unwrap_or(&0) + 1;
+                    modules_buildsystems_count.insert("unspecified".to_string(), new_buildsystem_count);
+                }
+
+                for source in &module_description.sources {
+                    sources_total_count += 1;
+
+                    if source.supports_mirror_urls() {
+                        sources_mirror_urls_supported_count += 1;
+                    }
+                    if source.get_all_mirror_urls().len() != 0 {
+                        sources_mirror_urls_available_count += 1;
                     }
 
-                    for source in &module_description.sources {
-                        sources_total_count += 1;
+                    let source_type_name = source.get_type_name();
+                    let new_count = sources_count.get(&source_type_name).unwrap_or(&0) + 1;
+                    sources_count.insert(source_type_name.to_string(), new_count);
 
-                        if source.supports_mirror_urls() {
-                            sources_mirror_urls_supported_count += 1;
-                        }
-                        if source.get_all_mirror_urls().len() != 0 {
-                            sources_mirror_urls_available_count += 1;
-                        }
+                    if source_type_name == "git" && source.has_commit() {
+                        sources_git_with_commit_count += 1;
+                    }
+                    if source_type_name == "git" && source.has_tag() {
+                        sources_git_with_tag_count += 1;
+                    }
+                    if source_type_name == "git" && source.has_tag() && source.has_commit() {
+                        sources_git_with_tag_and_commit_count += 1;
+                    }
 
-                        let source_type_name = source.get_type_name();
-                        let new_count = sources_count.get(&source_type_name).unwrap_or(&0) + 1;
-                        sources_count.insert(source_type_name.to_string(), new_count);
+                    if !source.type_is_valid() {
+                        invalid_sources_count += 1;
+                    }
 
-                        if source_type_name == "git" && source.has_commit() {
-                            sources_git_with_commit_count += 1;
-                        }
-                        if source_type_name == "git" && source.has_tag() {
-                            sources_git_with_tag_count += 1;
-                        }
-                        if source_type_name == "git" && source.has_tag() && source.has_commit() {
-                            sources_git_with_tag_and_commit_count += 1;
-                        }
+                    if source.type_is_empty() {
+                        empty_sources_count += 1;
+                    }
 
-                        if !source.type_is_valid() {
-                            invalid_sources_count += 1;
-                        }
-
-                        if source.type_is_empty() {
-                            empty_sources_count += 1;
-                        }
-
-                        for url in source.get_all_urls() {
-                            if source_type_name == "git" {
-                                log::debug!("GIT URL {}", url);
-                            } else if source_type_name == "archive" {
-                                log::debug!("ARCHIVE URL {}", url);
-                                sources_archives_count += 1;
-                                if fpm::utils::get_semver_from_archive_url(&url).is_some() {
-                                    sources_archives_with_semver += 1;
-                                }
-                                if fpm::utils::get_git_url_from_archive_url(&url).is_some() {
-                                    sources_archives_with_direct_git_url += 1;
-                                } else {
-                                    log::debug!("ARCHIVE URL FROM UNKNOWN SOURCE {}", url);
-                                }
+                    for url in source.get_all_urls() {
+                        if source_type_name == "git" {
+                            log::debug!("GIT URL {}", url);
+                        } else if source_type_name == "archive" {
+                            log::debug!("ARCHIVE URL {}", url);
+                            sources_archives_count += 1;
+                            if fpm::utils::get_semver_from_archive_url(&url).is_some() {
+                                sources_archives_with_semver += 1;
+                            }
+                            if fpm::utils::get_git_url_from_archive_url(&url).is_some() {
+                                sources_archives_with_direct_git_url += 1;
+                            } else {
+                                log::debug!("ARCHIVE URL FROM UNKNOWN SOURCE {}", url);
                             }
                         }
                     }
                 }
-
             }
+        }
 
-            if let Some(flatpak_module) = FlatpakModuleDescription::load_from_file(file_path.to_string()) {
-                standalone_modules_count += 1;
-            }
-
+        for manifest_path in &project.flatpak_module_manifests {
+            let absolute_manifest_path = repo_dir.to_string() + manifest_path;
+            let flatpak_module = FlatpakModuleDescription::load_from_file(absolute_manifest_path).unwrap();
+            standalone_modules_count += 1;
         }
     }
     println!("Unique app IDs: {}", app_ids.len());
