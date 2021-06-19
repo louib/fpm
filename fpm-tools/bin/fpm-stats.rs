1cb4b0ab973e0655b41fc3f7d0eb7d6398606a53
louib <code@louib.net>
2021-06-19 18:46:46 -0400

    Count exact git matches.

--- fpm-tools/bin/fpm-stats.rs
+++ fpm-tools/bin/fpm-stats.rs
@@ -20,6 +20,7 @@ fn main() {
     let mut sources_git_with_tag_count: i64 = 0;
     let mut sources_git_with_tag_and_commit_count: i64 = 0;
     let mut sources_archives_with_semver: i64 = 0;
+    let mut sources_archives_with_direct_git_url: i64 = 0;
     let mut sources_archives_count: i64 = 0;
     let mut invalid_sources_count: i64 = 0;
     let mut empty_sources_count: i64 = 0;
@@ -206,6 +207,9 @@ fn main() {
                                 if fpm::utils::get_semver_from_archive_url(&url).is_some() {
                                     sources_archives_with_semver += 1;
                                 }
+                                if fpm::utils::get_git_url_from_archive_url(&url).is_some() {
+                                    sources_archives_with_direct_git_url += 1;
+                                }
                             }
                         }
                     }
@@ -257,6 +261,7 @@ fn main() {
     println!("Git sources fixed with tag: {}% ({}/{})", (sources_git_with_tag_count as f64 / *sources_git_count as f64) * 100.0, sources_git_with_tag_count, sources_git_count);
     println!("Git sources fixed with tag and commit: {}% ({}/{})", (sources_git_with_tag_and_commit_count as f64 / *sources_git_count as f64) * 100.0, sources_git_with_tag_and_commit_count, sources_git_count);
     println!("Archive URLS with a semver: {}% ({}/{})", (sources_archives_with_semver as f64 / sources_archives_count as f64) * 100.0, sources_archives_with_semver, sources_archives_count);
+    println!("Archive URLS with a direct git repository: {}% ({}/{})", (sources_archives_with_direct_git_url as f64 / sources_archives_count as f64) * 100.0, sources_archives_with_direct_git_url, sources_archives_count);
     println!("Sources with invalid type: {}.", invalid_sources_count);
     println!("Sources with empty type: {}.", empty_sources_count);
     println!("=====================");
