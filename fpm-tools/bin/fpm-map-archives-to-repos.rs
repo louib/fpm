dfc3b000d00d515aa01e490bfd7709e25accfb1a
louib <code@louib.net>
2021-07-26 17:59:17 -0400

    Load urls from dump during mapping

--- fpm-tools/bin/fpm-map-archives-to-repos.rs
+++ fpm-tools/bin/fpm-map-archives-to-repos.rs
@@ -25,87 +25,52 @@ fn main() {
         panic!("There are no projects in the database!");
     }
 
-    let mut all_git_urls_from_manifests: HashSet<String> = HashSet::new();
-    let mut all_archive_urls: HashSet<String> = HashSet::new();
+    let mut git_urls_from_manifests: HashSet<String> = HashSet::new();
+    let mut archive_urls_from_manifests: HashSet<String> = HashSet::new();
 
-    for (project_id, project) in &db.indexed_projects {
-        // We're only interested in having stats for the projects supporting Flatpak.
-        if !project.supports_flatpak() {
-            continue;
-        }
-
-        log::info!("Processing project {}...", project_id);
-        let repo_url = project.get_main_vcs_url();
-
-        let repo_dir = match fpm::utils::clone_git_repo(&repo_url) {
-            Ok(d) => d,
-            Err(e) => {
-                eprintln!("Could not clone repo {}: {}", &repo_url, e);
-                continue;
-            }
-        };
-
-        for manifest_path in &project.flatpak_app_manifests {
-            let absolute_manifest_path = repo_dir.to_string() + manifest_path;
-
-            let flatpak_manifest = match FlatpakManifest::load_from_file(absolute_manifest_path.to_string()) {
-                Some(m) => m,
-                None => {
-                    log::warn!(
-                        "Could not parse Flatpak manifest at {}!!!",
-                        absolute_manifest_path
-                    );
-                    continue;
-                }
-            };
-
-            // FIXME we should get the modules recursively here!!!
-            for module in &flatpak_manifest.modules {
-                let module_description = match &module {
-                    FlatpakModule::Path(_) => continue,
-                    FlatpakModule::Description(d) => d,
-                };
-
-                for git_url in module_description.get_all_git_urls() {
-                    all_git_urls_from_manifests.insert(git_url.to_string());
-                }
-                for archive_url in module_description.get_all_archive_urls() {
-                    // FIXME remove this after testing!!!
-                    if all_archive_urls.len() > 20 {
-                        continue;
-                    }
-                    all_archive_urls.insert(archive_url.to_string());
-                }
-            }
-        }
-
-        for manifest_path in &project.flatpak_module_manifests {
-            let absolute_manifest_path = repo_dir.to_string() + manifest_path;
-            let module_description =
-                FlatpakModuleDescription::load_from_file(absolute_manifest_path).unwrap();
-            for git_url in module_description.get_all_git_urls() {
-                all_git_urls_from_manifests.insert(git_url.to_string());
-            }
-            for archive_url in module_description.get_all_archive_urls() {
-                all_archive_urls.insert(archive_url.to_string());
-            }
-        }
+    let git_urls_dump_path = format!("{}/git_urls_from_manifests.txt", fpm::db::Database::get_db_path());
+    let git_urls_dump_path = path::Path::new(&git_urls_dump_path);
+    if !git_urls_dump_path.is_file() {
+        panic!("Could not find git urls dump at {}.", git_urls_dump_path.to_str().unwrap());
     }
-
+    let git_urls_dump = match fs::read_to_string(git_urls_dump_path) {
+        Ok(content) => content,
+        Err(e) => panic!(e.to_string()),
+    };
+    for git_url in git_urls_dump.split("\n") {
+        git_urls_from_manifests.insert(git_url.to_string());
+    }
     log::info!(
-        "Extracted {} git urls from the manifests",
-        all_git_urls_from_manifests.len()
-    );
-    log::info!(
-        "Extracted {} archive urls from the manifests",
-        all_archive_urls.len()
+        "Loaded {} git urls from the manifests",
+        git_urls_from_manifests.len()
     );
 
-    for archive_url in &all_archive_urls {
+
+    let archive_urls_dump_path = format!(
+        "{}/archive_urls_from_manifests.txt",
+        fpm::db::Database::get_db_path()
+    );
+    let archive_urls_dump_path = path::Path::new(&archive_urls_dump_path);
+    if !archive_urls_dump_path.is_file() {
+        panic!("Could not find git urls dump at {}.", archive_urls_dump_path.to_str().unwrap());
+    }
+    let archive_urls_dump = match fs::read_to_string(archive_urls_dump_path) {
+        Ok(content) => content,
+        Err(e) => panic!(e.to_string()),
+    };
+    for archive_url in archive_urls_dump.split("\n") {
+        archive_urls_from_manifests.insert(archive_url.to_string());
+    }
+    log::info!(
+        "Loaded {} archive urls from the manifests",
+        archive_urls_from_manifests.len()
+    );
+
+    for archive_url in &archive_urls_from_manifests {
         if fpm::utils::get_semver_from_archive_url(&archive_url).is_none() {
             continue;
         }
-        if let Some(git_url) = get_git_url_for_archive(archive_url, &all_git_urls_from_manifests) {
+        if let Some(git_url) = get_git_url_for_archive(archive_url, &git_urls_from_manifests) {
             println!("Git URL for {} is {}.", archive_url, git_url);
         } else {
             println!("Could not find git URL for {}.", archive_url);
