352ffd2445b0f468f38d51ff0e3081484b1b327b
louib <code@louib.net>
2021-10-20 08:37:48 -0400

    Starting to import modules.

--- src/bin/fpm-import-modules.rs
+++ src/bin/fpm-import-modules.rs
@@ -1,3 +1,24 @@
+use std::env;
+use std::path;
 
 fn main() {
+    fpm::logger::init();
+
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        panic!("Requires 1 argument: the path of the directory to use for the import.");
+    }
+
+    let path = &args[1];
+
+    let file_paths = match fpm::utils::get_all_paths(path::Path::new(path)) {
+        Ok(paths) => paths,
+        Err(message) => {
+            eprintln!("Could not get the file paths :sad: {}", message);
+            return;
+        }
+    };
+    for file_path in file_paths.iter() {
+
+    }
 }
