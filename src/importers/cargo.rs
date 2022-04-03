5ffa2d37b1513145f7dfc8d0c0d8dcee63764bbd
louib <code@louib.net>
2022-04-02 20:58:30 -0400

    Basic Cargo.lock struct

--- src/importers/cargo.rs
+++ src/importers/cargo.rs
@@ -2,6 +2,18 @@ use flatpak_rs::module::FlatpakModule;
 
 pub const CRATES_IO_URL: &str = "https://static.crates.io/crates";
 
+pub struct CargoLock {
+    pub version: String,
+}
+
+pub struct CargoLockPackage {
+    pub name: String,
+    pub source: String,
+    pub checksum: String,
+    pub version: String,
+    pub dependencies: Vec<String>,
+}
+
 pub fn get_modules_from_manifest(manifest_path: &str) -> Vec<FlatpakModule> {
     vec![]
 }
